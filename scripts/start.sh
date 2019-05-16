0f8535c806e7b5bcc685ae71f89ae2f77cdc75bb
Florian Zouhar <florian.zouhar@igd.fraunhofer.de>
2019-05-16 12:19:39 +0200

    scripts no longer needed

--- scripts/start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -server $JAVA_OPTS -XX:+UnlockExperimentalVMOptions -XX:InitialRAMFraction=2 -XX:MinRAMFraction=2 -XX:MaxRAMFraction=2 -XX:+UseG1GC -XX:MaxGCPauseMillis=100 -XX:+UseStringDeduplication -jar tank-fat.jar $@
